d495fc9daf3eab3cf443d2ab41d04e090bd6fc5c
“Rameshwar <rameshwar.singh@svvatech.com>
2024-02-07 17:44:55 +0530

    Updates regarding session 1 and session 2

---
+++ 01_basics.js
@@ -0,0 +1,77 @@
+/* The directive looks like a string: "use strict" or 'use strict'.
+When it is located at the top of a script, 
+the whole script works the “modern” way. */
+
+"use strict";
+
+//below line will generate an error as we are running js using node not using browser
+
+// alert("hello world");
+
+// VARIABLES *********************
+console.log("***************** Variables Related  ********************");
+
+/* Variable declaration using var or let
+A variable is a “named storage” for data. 
+We can use variables to store goodies, visitors, and other data.
+Variable name must contain only letters, 
+digits, or the symbols $ and _ and the 
+first character must not be a digit.
+*/
+
+let message;
+
+message = "Hi welcome to SDC @ LKCTC";
+
+console.log(message);
+
+let user = "Rahul",
+  age = 25,
+  notice = "Hello";
+
+let studentClass = "MCA";
+
+var studentName = "Rameshwer";
+
+// Below is the example of creating variable without use of var or let
+// in case of 'use strict' mode we cant use following bad way of declaring a variable
+//This way is a bad way and would cause an error in strict mode:
+
+// studentAddress = "Jalandhar";
+
+// its preferable to use let instead of var due to problem of the block scope of the variable
+
+studentName = "Shyam";
+
+console.table([studentName, studentClass]);
+
+//CONSTANTS
+
+console.log("***************** Constants Related  ********************");
+
+// Constant declaration using const keyword
+
+// Constant can not be reassigned a new value.
+
+const studentRollNumber = 101;
+
+const BLOCK_D = "D";
+
+// in the below line we are trying to assign a new value to cost type but it will generate the error
+
+// studentRollNumber = 102;
+
+console.log(BLOCK_D);
+
+//Good convention to declare constants with CAPITAL letters like below
+
+const PI = 3.14;
+
+console.log(PI);
+
+/*
+Some Notes
+let – represents a contemporary method of declaring variables.
+var – signifies a traditional approach to variable declaration. It is generally avoided in modern coding practices, but we'll explore nuanced distinctions from let in the section titled "The Legacy of 'var,' just in case such nuances become relevant."
+const – mirrors let in its declaration style, but with the added constraint that the 
+*/
---
+++ 02_datatypes.js
@@ -0,0 +1,237 @@
+"use strict";
+
+/*
+
+In JavaScript, every value is associated with a specific data type, such as a string or a number.
+
+JavaScript encompasses eight fundamental data types.
+
+undefined
+null
+number
+bigint
+string
+boolean
+symbol
+object
+*/
+
+// NUMBER
+console.log("***************** Number Datatype ********************");
+
+let marksEnglish = 23;
+marksEnglish = 25.8;
+
+// There are different operations that can be performed on number datatype i.e. add, subtract, divide ...
+
+//Apart from number some other possible values that number can hold are NaN, -Infinity, Infinity
+
+console.log(1 / 0); // It will output "Infinity"
+
+console.log("TWO" / 2); // This will result in NaN i.e. not a number
+
+// Some more NaN examples are
+
+console.log(NaN + 100); // NaN
+console.log(300 * NaN); // NaN
+console.log("not a number" / 20 - 10); // NaN
+
+console.log(NaN ** 0); // Exception it will result to 1
+
+/* 
+
+If a NaN (Not-a-Number) appears within a mathematical expression, it extends its influence to the entire result. 
+It's important to note that there is only one exception to this rule: NaN ** 0 evaluates to 1.
+
+*/
+
+// BIGINT
+
+console.log("***************** BigInt Datatype ********************");
+
+/* 
+
+In JavaScript, the "number" type faces limitations when accurately 
+representing integer values beyond (2**53 - 1) or below -(2**53 - 1) 
+for negatives. 
+
+*/
+
+//Values beyond (2**53 - 1) cant be stored in normal number type.
+
+console.log(9007199254740991 + 1); // will be = 9007199254740992
+console.log(9007199254740991 + 2); // will result in = 9007199254740992
+
+//To overcome above limitation BigInt is is added in js
+
+//To declare a number as BigInt just add n at the end of the number like 1234567890123456789012345678901234567890n
+
+let bigIntExample = 1234567890123456789012345678901234567890n;
+
+console.log(bigIntExample);
+
+/* 
+Note
+
+As of now, BigInt is compatible with Firefox, Chrome, Edge, and Safari; however, 
+it is not supported in Internet Explorer.
+
+*/
+
+// STRING
+console.log("***************** String Datatype ********************");
+
+// A string JS is the value enclosed with in quotes
+//"" or '' and special use of ``.
+
+let fName = "Rameshwer";
+
+let lName = "Singh";
+
+let age = 35;
+
+// Backticks serve as quotes with "extended functionality."
+// They enable the incorporation of variables and expressions into a string,
+// achieved by enclosing them within ${...}.
+
+let introduction = `My name is ${fName} and my age is ${age + 5}`;
+
+console.log(introduction);
+
+console.log("***************** Boolean Datatype ********************");
+
+// Boolean type can have only two values i.e. true false
+
+let termsAccepted = true;
+
+let termsRejected = false;
+
+console.log(10 > 10); // will evaluate to false
+
+console.log(10 === 10); // will evaluate to false
+
+console.log("***************** Null Datatype ********************");
+
+/*
+
+
+In JavaScript, null is a primitive data type that 
+represents the intentional absence of any object value. 
+It is often used to signify the absence of a 
+value or to indicate that a variable has no assigned value
+
+*/
+
+let subject = null;
+
+console.log(subject);
+
+console.log("***************** Undefined Datatype ********************");
+
+//The term "undefined" in JavaScript signifies that a value has not been assigned.
+// When a variable is declared without an assigned value, it defaults to being undefined.
+
+let myAge;
+console.log(myAge);
+
+console.log("***************** Object Datatype ********************");
+
+/*
+Objects are distinguished from other types as "primitive"
+ because their values have the ability to encompass more than just a single element, 
+ such as a string or a number. Unlike other types that can only hold individual values, 
+objects serve as containers for organizing and managing collections of data and more intricate entities.
+*/
+
+let studentDetails = {
+  name: "Ram",
+  class: "B.Tech",
+  email: "ram@sham.com",
+  age: 22,
+};
+
+console.log(studentDetails);
+
+console.log("***************** Symbol Datatype ********************");
+/*
+
+In JavaScript, the Symbol type is a primitive data type introduced in ECMAScript 6 (ES6). 
+Symbols are unique and immutable values that can be used as identifiers for object properties. 
+They are often employed to create private or hidden properties within objects and to avoid naming conflicts.
+Symbols are created using the Symbol() function, and each symbol is guaranteed to be unique. Here's a simple example:
+
+*/
+
+// Creating a symbol
+const mySymbol = Symbol();
+
+// Symbols can also have a description (optional)
+const namedSymbol = Symbol("myDescription");
+
+// Using symbols as property keys
+const obj = {};
+obj[mySymbol] = "Hello, Symbol!";
+obj[namedSymbol] = "Symbol with description";
+
+console.log(obj[mySymbol]); // Output: Hello, Symbol!
+console.log(obj[namedSymbol]); // Output: Symbol with description
+
+console.log("***************** typeof Operator ********************");
+
+/* 
+The typeof operator is employed to determine the data type stored in a variable. 
+Typically written as typeof x, it can also be expressed as typeof(x). 
+The operator yields a string indicating the type of the variable, such as "string." However, 
+when applied to a null value, it incorrectly returns "object"; 
+this is considered a language flaw, as null is not actually an object.
+*/
+
+let variableA; // undefined variable
+
+console.log(typeof variableA);
+// Output: "undefined"
+// Explanation: The typeof operator returns "undefined" for an uninitialized variable.
+
+let numberB = 42; // a number
+
+console.log(typeof numberB);
+// Output: "number"
+// Explanation: The typeof operator returns "number" for a numeric value.
+
+let bigIntC = 123n; // a BigInt
+
+console.log(typeof bigIntC);
+// Output: "bigint"
+// Explanation: The typeof operator returns "bigint" for a BigInt value.
+
+let booleanD = false; // a boolean
+
+console.log(typeof booleanD);
+// Output: "boolean"
+// Explanation: The typeof operator returns "boolean" for a boolean value.
+
+let stringE = "bar"; // a string
+
+console.log(typeof stringE);
+// Output: "string"
+// Explanation: The typeof operator returns "string" for a string value.
+
+let symbolF = Symbol("key"); // a symbol
+
+console.log(typeof symbolF);
+// Output: "symbol"
+// Explanation: The typeof operator returns "symbol" for a symbol value.
+
+let objectG = { key: "value" }; // an object
+
+console.log(typeof objectG);
+// Output: "object"
+// Explanation: The typeof operator returns "object" for an object.
+
+let functionH = function () {
+  alert("Hello!");
+}; // a function
+
+console.log(typeof functionH);
+// Output: "function"
+// Explanation: The typeof operator returns "function" for a function.
